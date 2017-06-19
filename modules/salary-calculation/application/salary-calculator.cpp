171d19b7e1689165bbd4dd363f6fb1a0aeec9e3e
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-06-19 14:47:22 +0300

    +App

---
+++ modules/salary-calculation/application/salary-calculator.cpp
@@ -0,0 +1,12 @@
+// Copyright 2017 Korniakov Kirill
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "../salary-calculation/include/salary_caculationl.h"
+
+int main(int argc, const char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
