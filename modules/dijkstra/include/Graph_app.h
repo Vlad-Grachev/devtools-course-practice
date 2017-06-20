e6d2f64db494dc4803252072aaab643bcf8a1f3d
Ilya Kozlov <ilya140297@gmail.com>
2017-06-20 17:39:43 +0300

    format fix 1

--- modules/dijkstra/include/Graph_app.h
+++ modules/dijkstra/include/Graph_app.h
@@ -1,9 +1,13 @@
 // Copyright 2017 Kozlov Ilya
- 
+
+#ifndef  MODULES_DIJKSTRA_INCLUDE_GRAPH_APP_H_
+#define MODULES_DIJKSTRA_INCLUDE_GRAPH_APP_H_
+
 #include "include\Graph.h"
 
 class GraphApp
 {
-public:
-protected:
+ public:
+ protected:
 };
+#endif // ! MODULES_DIJKSTRA_INCLUDE_GRAPH_APP_H_
