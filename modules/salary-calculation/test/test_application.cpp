d6545f7b3174a55389bae3a3cfec55ef1e959a10
Umlore1998 <iv.kiselev1998@yandex.ru>
2017-06-23 18:39:38 +0300

    Test 8 - GREEN

--- modules/salary-calculation/test/test_application.cpp
+++ modules/salary-calculation/test/test_application.cpp
@@ -22,14 +22,14 @@ TEST(Application, Constructor_copir) {
 
 TEST(Application, Operator) {
     Application app;
-    char** chr = new char*[2];
-    chr[0] = new char[1];
-    chr[0][0] = '7';
+    char** chr = new char*[3];
     chr[1] = new char[1];
-    chr[1][0] = '1';
+    chr[1][0] = '7';
+    chr[2] = new char[1];
+    chr[2][0] = '1';
     Salary sal;
     sal.CalculateSalary();
-    EXPECT_EQ(app(4, chr), 3200);
+    EXPECT_EQ(app(3, chr), 3200);
 }
 
 TEST(Application, Char_To_Int_1) {
@@ -45,30 +45,30 @@ TEST(Application, Char_To_Int_2) {
 }
 
 TEST(Application, Hard_Test) {
-    char** arr = new char*[14];
+    char** arr = new char*[15];
     Application app;
-    for (int i = 0; i <= 12; i += 2) {
+    for (int i = 1; i <= 13; i += 2) {
         arr[i] = new char[1];
-        arr[i][0] = '0' + i / 2 + 1;
+        arr[i][0] = '0' + ( i - 1 ) / 2 + 1;
     }
-    arr[1] = new char[2];
-    arr[1][0] = '5';
-    arr[1][1] = '0';
-    arr[3] = new char[2];
-    arr[3][0] = '3';
-    arr[3][1] = '0';
-    arr[5] = new char[2];
-    arr[5][0] = '6';
-    arr[5][1] = '0';
-    arr[7] = new char[1];
-    arr[7][0] = '0';
-    arr[9] = new char[2];
-    arr[9][0] = '2';
-    arr[9][1] = '0';
-    arr[11] = new char[1];
-    arr[11][0] = '8';
-    arr[13] = new char[1];
-    arr[13][0] = '0';
+    arr[2] = new char[2];
+    arr[2][0] = '5';
+    arr[2][1] = '0';
+    arr[4] = new char[2];
+    arr[4][0] = '3';
+    arr[4][1] = '0';
+    arr[6] = new char[2];
+    arr[6][0] = '6';
+    arr[6][1] = '0';
+    arr[8] = new char[1];
+    arr[8][0] = '0';
+    arr[10] = new char[2];
+    arr[10][0] = '2';
+    arr[10][1] = '0';
+    arr[12] = new char[1];
+    arr[12][0] = '8';
+    arr[14] = new char[1];
+    arr[14][0] = '0';
     int res = app(15, arr);
     EXPECT_EQ(res, 1600);
 }
