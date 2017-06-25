6c98b0f72d3e1d995121cb031c975f9367dbcc91
Ivan Kochin <ivan.kochin@inbox.ru>
2017-06-25 16:44:32 +0300

    last update4

--- modules/volume-calculator/src/volume_calculator_app.cpp
+++ modules/volume-calculator/src/volume_calculator_app.cpp
@@ -72,7 +72,7 @@ int parseInt(const char* arg) {
 
 
 bool VolumeCalculator::validateNumberOfArguments(int argc, const char** argv) {
-    __int16 mode;
+    __int8 mode;
     if (argc == 1) {
         help(argv[0]);
         return false;
