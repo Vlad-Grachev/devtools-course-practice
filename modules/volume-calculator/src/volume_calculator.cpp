6effeefdbe5d13bded172e2432b77182b065a03d
Ivan Kochin <ivan.kochin@inbox.ru>
2017-06-27 20:31:00 +0300

    code style fixed for Zemlyansky

--- modules/volume-calculator/src/volume_calculator.cpp
+++ modules/volume-calculator/src/volume_calculator.cpp
@@ -23,72 +23,72 @@ double Volume::Dodecahedron(double a) {
 double Volume::Cylinder(double h, double r) {
     if (h <= 0 || r <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return (M_PI*pow(r, 2)*h);
+    return M_PI * pow(r, 2) * h;
 }
 
 double Volume::Parallelepiped(double S, double h) {
     if (S <= 0 || h <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return S*h;
+    return S * h;
 }
 
 double Volume::Parallelepiped(double a, double b, double h) {
     if (a <= 0 || b <= 0 || h <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return a*b*h;
+    return a * b * h;
 }
 
 double Volume::Parallelepiped(double a, double b, double c, double x) {
     if (a <= 0 || b <= 0 || c <= 0 || sin(x) <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    double res = a*b*c*sin(x);
+    double res = a * b * c * sin(x);
     return res;
 }
 
 double Volume::Pyramid(double S, double h) {
     if (S <= 0 || h <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return S*h/3;
+    return S * h / 3;
 }
 
 double Volume::Pyramid(double a, double base_h, double pyramid_h) {
     if (a <= 0 || base_h <= 0 || pyramid_h <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return (a*base_h/2)*pyramid_h/3;
+    return a * base_h / 2 * pyramid_h / 3;
 }
 
 double Volume::Cone(double h, double r) {
     if (h <= 0 || r <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return M_PI*pow(r, 2)*h/3;
+    return M_PI * pow(r, 2) * h/3;
 }
 
 double Volume::Sphere(double r) {
     if (r <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return 4/3*M_PI*pow(r, 3);
+    return 4 / 3 * M_PI * pow(r, 3);
 }
 
 double Volume::Ellipsoid(double a, double b, double c) {
     if (a <= 0 || b <= 0 || c <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return 4/3*M_PI*a*b*c;
+    return 4 / 3 * M_PI * a * b * c;
 }
 
 double Volume::Paraboloid(double h, double r) {
     if (h <= 0 || r <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return M_PI*pow(r, 2)*h/2;
+    return M_PI * pow(r, 2) * h / 2;
 }
 
 double Volume::RectangularParallelepiped(double a, double b, double c) {
     if (a <= 0 || b <= 0 || c <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return a*b*c;
+    return a * b * c;
 }
 
 double Volume::Frustum(double r_base_1, double r_base_2, double h) {
     if (r_base_1 <= 0 || r_base_2 <= 0 || h <= 0)
         throw std::string("error of class \"Volume\": incorrect arguments");
-    return M_PI*h*(pow(r_base_1, 2)+r_base_1*r_base_2+pow(r_base_2, 2))/3;
+    return M_PI * h * (pow(r_base_1, 2) + r_base_1 * r_base_2 + pow(r_base_2, 2)) / 3;
 }
