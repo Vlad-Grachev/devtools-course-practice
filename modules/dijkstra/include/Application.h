2eb49b081ceeff9e0cd2cf3a37728b5710f61f22
Ilya Kozlov <ilya140297@gmail.com>
2017-06-28 05:17:11 +0300

    упростил для себя код добавив новый метод в Application

--- modules/dijkstra/include/Application.h
+++ modules/dijkstra/include/Application.h
@@ -15,19 +15,13 @@ class Application {
 
  private:
     void Help(const char* appname, const char* message = "");
+    vector<vector<unsigned int>> ParseGraph(const int argc, const char** argv);
     int ParseInt(const char* arg);
     bool ValidateNumberOfArguments(int argc, const char** argv);
     std::string message_;
     typedef struct {
         int start_node_;
-        int size_;
-        int connected_nodes_;
-        std::vector<unsigned int> edges_;
-    } Arguments;
-    typedef struct {
-        int weight_;
-        int start_;
-        int end_;
-    } Connection;
+        std::vector <std::vector<unsigned int>> gm_;
+    } Arguments
 };
 #endif  // MODULES_DIJKSTRA_INCLUDE_APPLICATION_H_
--- modules/dijkstra/src/Application.cpp
+++ modules/dijkstra/src/Application.cpp
@@ -7,8 +7,10 @@
 #include <string>
 #include <sstream>
 #include <vector>
-
+#include "include/Application.h"
 #include "include/Graph.h"
+
+using std::vector;
 
 Application::Application() {}
 
@@ -29,7 +31,6 @@ void Application::Help(const char* appname, const char* message) {
         "\n*The number of arguments should be > 2 and 2 + n, where the 'n' is a multiple of 3!" +
         "\n*Carefully observe the format where typing the edges!";
 };
-
 int Application::ParseInt(const char* arg) {
     try {
         return std::stoi(arg);
@@ -38,7 +39,21 @@ int Application::ParseInt(const char* arg) {
         throw str;
     }
 }
-
+vector<vector<unsigned int>> Application::ParseGraph(const int argc, const char** argv) {
+    const int gr_size = argc - 3;
+    vector<vector<unsigned int>> res (gr_size);
+    for (int i = 0; i < res.size(); i++) {
+        res[i].resize(3);
+    }
+    for (int i = 3; i < gr_size; i++) {
+        if (i % 3 == 0) {
+            res[i][0] = ParseInt(argv[i]);
+            res[i][1] = ParseInt(argv[i + 1]);
+            res[i][2] = ParseInt(argv[i + 2]);
+        }
+    }
+    return res;
+}
 bool Application::ValidateNumberOfArguments(int argc, const char** argv) {
     if (argc == 1) {
         Help(argv[0]);
@@ -55,6 +70,7 @@ bool Application::ValidateNumberOfArguments(int argc, const char** argv) {
 
 std::string Application::operator()(int argc, const char** argv) {
     Arguments args;
+    vector<unsigned int> res;
     std::ostringstream stream;
 
 
@@ -62,30 +78,25 @@ std::string Application::operator()(int argc, const char** argv) {
         return message_;
     }
     try {
-        args.size_ = ParseInt(argv[1]);
         args.start_node_ = ParseInt(argv[2]);
-        args.connected_nodes_ = (argc - 3) / 3;
-        for (int i = 3; i < args.connected_nodes_; i++) {
-            args.edges_.push_back(ParseInt(argv[i]));
-        }
+        args.gm_ = ParseGraph(argc, argv);
     }
     catch (std::string& str) {
         return str;
     }
-
-    Graph graph(args.size_);
-    for (int i = 0; i < args.connected_nodes_ * 3; i += 3) {
-        graph.AddEdge(args.edges_[i], args.edges_[i + 1], args.edges_[i + 2]);
+    Graph gr(args.gm_.size());
+    for (int i = 0; i < args.gm_.size(); i++) {
+        gr.AddEdge(args.gm_[i][0], args.gm_[i][1], args.gm_[i][2]);
     }
-    vector<unsigned int> res = graph.Dijkstra(args.start_node_);
-    for (unsigned int i = 0; i < graph.GetSize(); i++) {
-        if (res[i] < INF) {
-            stream << res[i];
+    res = gr.Dijkstra(args.start_node_);
+    for (int i = 0; i < res.size(); i++) {
+        if (res[i] == INF) {
+            stream << "inf ";
         }
-        else if (res[i] >= INF) {
-            stream << "inf";
+        if (res[i] != INF) {
+            stream << res[i] << " ";
         }
-        message_ += stream.str() + " ";
+        message_ += stream.str();
     }
     message_.pop_back();
     return message_;
